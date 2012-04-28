86dcbc7ccf104d3648a43e6123f488c70121ca78
fvosfdujsfgurjh <nikolas987@rambler.ru>
2012-04-28 19:41:35 +0400

    first commit!

---
+++ HotelWebApplication/HotelWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Account()
+        {
+            return View();
+        }
+
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Rooms()
+        {
+            return View();
+        }
+    }
+}
---
+++ HotelWebApplication/HotelWebApplication/Views/Home/Rooms.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/MasterPage.Master" Inherits="System.Web.Mvc.ViewPage<dynamic>" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+	Rooms
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h2>Rooms</h2>
+
+</asp:Content>
+
